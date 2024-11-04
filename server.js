f739f57e918f58dd6ca41995a986c4be1e9e1694
Edgar Felix <eaafelix@gmail.com>
2024-11-04 16:07:00 +0000

    Add server.js

---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const Init = async () => {
+  // Create Server
+  const Server = new Hapi.Server({
+    host: 'localhost',
+    port: 9000
+  });
+
+  // Register Plugins
+  await Server.register(require('@hapi/vision'));
+  await Server.register(require('@hapi/inert'));
+
+  // Register Services
+  await Server.register(require('./lib/services'));
+
+  // Register Routes
+  await Server.register(require('./lib/routes'));
+
+  // Views Configuration
+  Server.views({
+    engines: {
+      html: require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: './views',
+    layoutPath: './views/layouts',
+    layout: 'default',
+    isCached: false
+  });
+
+  // Start Server
+  await Server.start();
+
+  console.log('[NOTICE] Server running at', Server.info.uri);
+}
+
+Init();
